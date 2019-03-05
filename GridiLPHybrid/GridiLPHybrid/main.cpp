06fa41fde0f1eae31d02358f5bbe206f7a0e8e9f
Stephan Patterson <stephan.patterson@ucdenver.edu>
2019-03-05 12:44:56 -0700

    Adding Hybrid "i" version

---
+++ GridiLPHybrid/GridiLPHybrid/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GridiLPHybrid
+//
+//  Created by Stephan Patterson on 3/4/19.
+//  Copyright © 2019 Stephan Patterson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
